ad45c0dd2538d88453e5a81915c6ed3b5beecf8e
mcajongbah <malvinajongbah@gmail.com>
2022-02-28 12:09:19 +0000

    setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="w-full h-screen bg-purple-400 flex items-center justify-center">
+      <h1 className="text-4xl font-semibold text-white">Welcome to the boiler-plate</h1>
+    </div>
+  )
+}
+
+export default App
